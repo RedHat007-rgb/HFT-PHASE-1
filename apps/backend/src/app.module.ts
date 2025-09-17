60dd6a97a912683a71557cea7d2340c52b4a1502
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-17 13:23:40 +0530

    modified

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AppDataSource } from '@repo/dbschema';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      ...AppDataSource.options,
+      migrationsRun: true,
+      synchronize: false,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
