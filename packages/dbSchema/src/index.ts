7e238d6f89aa62ef86704fbe2700ad4b40203766
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-17 09:10:30 +0530

    Added exports

---
+++ packages/dbSchema/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./config/data-source";
+
+// ......................Entities..........................
+export * from "./entities/Base.entity";
+export * from "./entities/Market.entity";
+export * from "./entities/Orderbook.entity";
+export * from "./entities/Trade.entity";
