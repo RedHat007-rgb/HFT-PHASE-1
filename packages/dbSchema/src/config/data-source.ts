73f18da5a698e07d192692e63190efb753b1bc56
RedHat007-rgb <cu.16bcs1288@gmail.com>
2025-09-17 10:15:05 +0530

    modified path

--- packages/dbSchema/src/config/data-source.ts
+++ packages/dbSchema/src/config/data-source.ts
@@ -1,5 +1,10 @@
 import { DataSource } from "typeorm";
+import * as dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.DATABASE_USERNAME);
 export const AppDataSource = new DataSource({
   type: "postgres",
   url: `postgres://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DOCKER_DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+  migrations: [__dirname + "/../migrations/*.{ts,js}"],
+  entities: [__dirname + "/../entities/*.{ts,js}"],
 });
